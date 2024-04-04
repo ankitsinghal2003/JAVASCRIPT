0509450459b563662721775bbe42dd942a5fdaa7
ankitsinghal2003 <149244090+ankitsinghal2003@users.noreply.github.com>
2024-04-04 17:11:28 +0530

    Add files via upload

---
+++ Datatype.js
@@ -0,0 +1,70 @@
+//"use strict"; //iska mtlb h ki aaj ke time pe js ka new version of code use karege so treat all js code as newer version.
+
+// we know ki js ka engine pura browser ke ander chupa hua history.
+
+////////////////////Here we study datatype conversion confusing /////////////////////////////////////////
+
+//let history= "ankit123"
+//console.log(history)
+//console.log(typeof(history))
+//let changetype=Number(history)
+//console.log(typeof changetype);
+//console.log(changetype)
+
+
+// let loggedIn= null
+//console.log(typeof loggedIn)
+// let changebollen= Boolean(loggedIn) 
+// console.log( changebollen);
+//Explation--->
+// 1-> true 
+// 0 -> false
+//"" ->  false
+// null is a object convert into  boolean it will be false.
+// "gangapurcity" -> true
+
+
+//some more pratice 
+// let num ="567";
+// console.log( Number(num) ); //true
+// console.log(typeof Number(num) ) ; //number
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("ankit")
+//  accountId=12345
+const account=121212
+var mail="singhalankit729@gmail.com";
+ accountmail=123456789;
+// console.log(accountId)
+console.table([mail,account,accountmail])
